2e3ec444c3f5140c060b16273832f4525e65ad96
ZachRichter <richterzach15@gmail.com>
2016-10-15 15:11:56 -0500

    fixing me being stupid

--- Color.js
+++ Color.js
@@ -3,8 +3,7 @@ alicorn.shard("Color")
     .create(function(self) {
         sdk = self.require("AndroidFTC");
         joy1 = sdk.get("gamepad_1");
-      )}
-
-      .update(function(self) {
+    })
+    .update(function(self) {
         console.log(sdk.get("color"));
-      }
+    });

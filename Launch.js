f79a3aecf94990d40502814f8aec5bd66b706ec1
ZachRichter <richterzach15@gmail.com>
2016-10-20 16:45:54 -0500

    added program to launch crossbow

---
+++ Launch.js
@@ -0,0 +1,35 @@
+var joy1, sdk;
+alicorn.shard("Launch")
+
+  .create(function(self){
+
+    sdk = self.require("AndroidFTC");
+    joy1 = sdk.get("gamepad_1");
+
+  })
+
+  .update(function(self){
+
+    if(joy1.get("a") == true){
+      var a = true;
+    }
+    if(joy1.get("b") == true){
+      var b = true;
+    }
+
+    if(a == true){
+      sdk.set("cross", -0.3);
+      self.sleep(1300);
+      sdk.set("cross", 0);
+      self.sleep(1000);
+      a = false;
+    }
+
+    if(joy1.get("b") == true){
+      sdk.set("cross", 0.5);
+      self.sleep(1000);
+      sdk.set("cross", 0);
+      self.sleep(1000);
+      b = false;
+    }
+  });

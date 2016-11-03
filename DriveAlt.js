c0a7e3d5c512b46f5c64807937789fd8147e5926
ZachRichter <richterzach15@gmail.com>
2016-11-03 15:47:27 -0500

    added crossbow to drive

--- DriveAlt.js
+++ DriveAlt.js
@@ -10,8 +10,31 @@ alicorn.shard("DriveAlt")
 
   .update(function(self){
     var left = joy1.get("y1")/1.5;
-    var right = joy1.get("y2")/1.5;
+    var right = joy1.get("y2")/-1.5;
 
     sdk.set("left", left);
     sdk.set("right", right);
+
+
+    if(joy1.get("x") == true){
+      var a = true;
+    }
+    if(joy1.get("y") == true){
+      var b = true;
+    }
+
+    if(a == true){
+      sdk.set("cross", -0.3);
+      self.sleep(1000);
+      sdk.set("cross", 0);
+      self.sleep(1000);
+      a = false;
+    }
+
+    if(b == true){
+      sdk.set("cross", 0.5);
+      self.sleep(1000);
+      sdk.set("cross", 0);
+      self.sleep(1000);
+      b = false;
   });

1b37b52198c210ffb47a021a7e160dd4c43eecee
ZachRichter <richterzach15@gmail.com>
2016-11-15 14:59:31 -0600

    Changed Color to an autonomous

--- AutonColor.js
+++ AutonColor.js
@@ -2,6 +2,7 @@ var sdk, joy1;
 alicorn.shard("Color")
 
   .create(function(self){
+    console.log("create called");
 
     sdk = self.require("AndroidFTC");
     joy1 = sdk.get("gamepad_1");
@@ -9,7 +10,7 @@ alicorn.shard("Color")
 
     })
     .update(function(self) {
-
+        console.log("update called");
 
         for (var i =0; i >= 1; i++){
           console.log(sdk.get("color"));

3f859ad2ac8640902cd784aa08cf06bd4a77b337
ZachRichter <richterzach15@gmail.com>
2016-11-29 16:11:27 -0600

    Fixed launch
    based on a button being held down

--- Launch.js
+++ Launch.js
@@ -1,4 +1,4 @@
-var joy1, sdk, a, b;
+var joy1, sdk;
 alicorn.shard("Launch")
 
   .create(function(self){
@@ -11,23 +11,28 @@ alicorn.shard("Launch")
   .update(function(self){
 
     if(joy1.get("a") == true){
-      a = true;
+      var a = true;
     } else if (joy1.get("a") == false){
       a = false;
     }
+
     if(joy1.get("b") == true){
-      b = true;
+      var b = true;
     } else if (joy1.get("b") == false){
       b = false;
     }
-    while(a == true){
+
+
+    if(a == true){
       sdk.set("cross", -0.3);
-    }
-    while(b == true){
-      sdk.set("cross", 0.5);
-    }
-    while(a == false && b == false){
+    } else if(a == false && b == false){
       sdk.set("cross", 0);
     }
+    if(b == true){
+      sdk.set("cross", 0.5);
+    } else if(a == false && b == false){
+      sdk.set("cross", 0);
+    }
+
 
   });

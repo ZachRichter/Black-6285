0296f4bed0618d98a92c73f7236c91d4bf1250d8
ZachRichter <richterzach15@gmail.com>
2017-01-17 16:36:48 -0600

    More value changes
    changes for double shoot autonomous

--- AutonTest3.js
+++ AutonTest3.js
@@ -15,7 +15,7 @@ alicorn.shard("AutonTest3")
     sdk.set("cross", 0);
     self.sleep(800);
     sdk.set("cross", 0.5);
-    self.sleep(300);
+    self.sleep(700);
     sdk.set("cross", 0);
     self.sleep(500);
     sdk.set("chain", -1);
